9406629069d4929ae1c0aa882bcd80d645f37d83
unknown <romina.navea.rod@gmail.com>
2026-05-13 10:38:29 +0200

    reemplazos dataframe4 en queries y output

--- queries/Dataframe_3_Elenco_Popularidad.sql
+++ queries/Dataframe_3_Elenco_Popularidad.sql
@@ -1,16 +1,9 @@
-
-
-SELECT
+SELECT 
     f.film_id,
-    f.title AS Pelicula,
-    f.release_year AS Año_lanzamiento,
-    c.name AS Categoria,
-    l.name AS Idioma,
-    f.rating AS Clasificacion,
-    COUNT(DISTINCT i.inventory_id) AS Copias_en_inventario
-FROM film f
-INNER JOIN film_category fc ON f.film_id = fc.film_id
-INNER JOIN category c ON fc.category_id = c.category_id
-INNER JOIN language l ON f.language_id = l.language_id
-LEFT JOIN inventory i ON f.film_id = i.film_id
-GROUP BY f.film_id, f.title, f.release_year, c.name, l.name, f.rating;
+    f.title AS pelicula,
+    CONCAT(a.first_name, ' ', a.last_name) AS actor_completo,
+    COUNT(*) OVER(PARTITION BY a.actor_id) AS frecuencia_actor_total
+FROM film_actor fa
+JOIN film f ON fa.film_id = f.film_id
+JOIN actor a ON fa.actor_id = a.actor_id
+ORDER BY frecuencia_actor_total DESC, f.title;
